c4a0f72f387432e42cb9fe8f420ecd67a3137d0f
Anthony <sean.a.protho1@gmail.com>
2020-02-16 15:16:52 -0800

    breaking things with flask

--- app.py
+++ app.py
@@ -1,3 +1,16 @@
-#make env
-#make flask
-#don't forget to jinja template.
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+app.route('/')
+def home():
+    #The Cube
+    return render_template('home.html')
+
+app.route('/portfolio')
+def index():
+    #Portfolio
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=os.environ.get('PORT', 5000))
