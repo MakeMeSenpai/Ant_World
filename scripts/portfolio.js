37fbe3046861855d2c88155f8a3553919822b4c1
Anthony <sean.a.protho1@gmail.com>
2021-01-18 01:30:03 -0800

    progress balls

--- scripts/portfolio.js
+++ scripts/portfolio.js
@@ -1,8 +1,19 @@
+// progress balls
+const pb1 = document.getElementById("pb1");
+
 // progress bar
-window.onscroll = function() {
+window.onscroll = function () {
     var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
     var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
     var scrolled = (winScroll / height) * 100;
     document.getElementById("myBar").style.height = scrolled + "%";
+    console.log(scrolled) // checks what % is
+    // Then changes the value if scrolled above point
+    if (scrolled > 1.7) {
+        // changing background of progress balls
+        pb1.style.backgroundColor = "blue";
+    } else if (scrolled < 1.7) {
+        // to their respective colors
+        pb1.style.backgroundColor = "#00aeff";
+    }
 }
-
