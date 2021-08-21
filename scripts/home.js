ba097622a00db9ec35e61a6ff6694e926b9369b8
Anthony <sean.a.protho1@gmail.com>
2021-08-20 20:59:05 -0700

    SQUAH

--- scripts/home.js
+++ scripts/home.js
@@ -19,3 +19,23 @@ radioGroup.addEventListener('change', changeSide);
 
 // ANT___________
 var ant = document.querySelector('#ant');
+var animation = document.querySelector('#animate')
+
+function squashed() {
+  ant.style.animationPlayState = 'paused';
+  animation.setAttribute('src', "./static/imgs/squash.webm")
+  ant.load()
+  ant.play()
+  ant.loop = false;
+  ant.onended = function () {
+    console.log("You Squashed Me!")
+    animation.setAttribute('src', "./static/imgs/walk.webm")
+    ant.load()
+    ant.play()
+  };
+  setTimeout(() => {
+    ant.loop = true;
+    ant.style.animationPlayState = 'running';
+  }, 1000)
+  return
+}
